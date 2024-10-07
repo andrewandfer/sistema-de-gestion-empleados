fc1ed4fc65c2da3fd21e35735add176f11c95cfa
andrewandfer <fernelyandres13@gmail.com>
2024-10-06 19:02:41 -0500

    cambios

--- gestion documentos/src/main/java/co/edu/uniquindio/poo/App/Empresa.java
+++ gestion documentos/src/main/java/co/edu/uniquindio/poo/App/Empresa.java
@@ -1,6 +1,6 @@
 package co.edu.uniquindio.poo.App;
 
-public class Biblioteca {
+public class Empresa {
     public static void main(String[] args){
         // Aquí va la lógica central del programa, desde aquí comienza la ejecución de la JVM.
     }
